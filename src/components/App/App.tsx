4742fe1790a0e965d388764d3f214eeb56e47b56
SevenTrio <dimakrikunov12@gmail.com>
2021-11-27 20:19:44 +0200

    chore: initial project setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => (
+  <div className="App">
+    <h1>Hello world!</h1>
+  </div>
+);
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import App from './components/App/App';
+import './styles/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
