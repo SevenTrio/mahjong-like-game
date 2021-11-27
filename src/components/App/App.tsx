56dbb2a6df2d4086217a5a05e2b3745033a05075
SevenTrio <dimakrikunov12@gmail.com>
2021-11-27 22:44:40 +0200

    feat: application UI

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import MainLayout from 'layouts/MainLayout/MainLayout';
+import CardsContainer from 'components/CardsContainer/CardsContainer';
+import Card from 'components/Card/Card';
 import './App.scss';
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Hello world!</h1>
-  </div>
-);
+const App: React.FC = () => {
+  const cards = new Array(30).fill(1);
+
+  return (
+    <MainLayout>
+      <CardsContainer>
+        {cards.map((card, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Card key={index} isVisible>
+            {card}
+          </Card>
+        ))}
+      </CardsContainer>
+    </MainLayout>
+  );
+};
 
 export default App;
