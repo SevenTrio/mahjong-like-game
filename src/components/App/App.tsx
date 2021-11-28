c415c957875365e0e3fe13ca8b76ee5ed2e8927c
SevenTrio <dimakrikunov12@gmail.com>
2021-11-28 14:23:51 +0200

    feat: connect UI with logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import MainLayout from 'layouts/MainLayout/MainLayout';
-import CardsContainer from 'components/CardsContainer/CardsContainer';
-import Card from 'components/Card/Card';
-import './App.scss';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+import MainPage from 'pages/MainPage/MainPage';
 
-const App: React.FC = () => {
-  const cards = new Array(30).fill(1);
-
-  return (
-    <MainLayout>
-      <CardsContainer>
-        {cards.map((card, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Card key={index} isVisible>
-            {card}
-          </Card>
-        ))}
-      </CardsContainer>
-    </MainLayout>
-  );
-};
+const App: React.FC = () => (
+  <Provider store={store}>
+    <MainPage />
+  </Provider>
+);
 
 export default App;
